3351b64e0bc7dce7dbd3390477dd4633aafcf172
twistedttea <twistedtteavr@gmail.com>
2026-04-10 21:59:26 -0500

    Update flake to build on all systems

--- flake.nix
+++ flake.nix
@@ -4,18 +4,34 @@
     nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    packages.aarch64-darwin = {
+  outputs = {
+    self,
+    nixpkgs,
+    nvf,
+    ...
+  } @ inputs: let
+    # 1. Define the architectures you want to support
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    # 2. Create a helper function to iterate over those systems
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    # 3. Wrap your packages output with the helper
+    packages = forAllSystems (system: {
       # Set the default package to the wrapped instance of Neovim.
       # This will allow running your Neovim configuration with
-      # `nix run` and in addition, sharing your configuration with
-      # other users in case your repository is public.
+      # `nix run` and sharing it with other users.
       default =
-        (inputs.nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        (nvf.lib.neovimConfiguration {
+          # 4. Inject the current system dynamically
+          pkgs = nixpkgs.legacyPackages.${system};
           modules = [./config.nix];
-        })
-        .neovim;
-    };
+        }).neovim;
+    });
   };
 }
